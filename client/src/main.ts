11821d67c82a3c1cbfeb1f6ac0cd6baad7fe139e
Ruslan Sattarov <r8sattarov@mail.ru>
2024-02-04 17:28:56 +0500

    build: Установка vuetify - ui components

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
 import App from './app/App.vue';
+import { vuetify } from './vuetify';
 
-createApp(App).mount('#app');
+createApp(App).use(vuetify).mount('#app');
---
+++ client/src/vuetify.ts
@@ -0,0 +1,8 @@
+import { createVuetify } from 'vuetify';
+import { VCard } from 'vuetify/components/VCard';
+
+export const vuetify = createVuetify({
+    components: {
+        VCard,
+    },
+});
