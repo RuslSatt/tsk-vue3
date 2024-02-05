448242e26107e392f694aa956c09ec47fd78c615
Ruslan Sattarov <r8sattarov@mail.ru>
2024-02-05 17:34:55 +0500

    feat: Настройка подключения к базе данных

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT, () => {
+        console.log(`Server is started on the port = ${PORT}`);
+    });
 }
-bootstrap();
+
+start();
