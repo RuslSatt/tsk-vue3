edba2c1fcb0df00f20b6f3b87c216e602c9ad33a
Ruslan Sattarov <r8sattarov@mail.ru>
2024-02-06 19:54:48 +0500

    feat: Взаимодействие логина и регистрации с глобальным store
    Сохранение пользователя в store и localStorage

--- client/src/features/Auth/model/services/login.ts
+++ client/src/features/Auth/model/services/login.ts
@@ -1,10 +1,16 @@
 import { host } from '@/app/http';
+import { useUserStore } from '@/app/providers/store/store';
 
 export const login = async (email: string, password: string) => {
 	const response = await host.post('auth/login', {
 		email,
 		password
 	});
+
+	const store = useUserStore();
+
+	store.setUser(response.data.user);
+	localStorage.setItem('token', response.data.token);
 
 	return response.data;
 };
--- client/src/features/Auth/model/services/registration.ts
+++ client/src/features/Auth/model/services/registration.ts
@@ -1,4 +1,5 @@
 import { host } from '@/app/http';
+import { useUserStore } from '@/app/providers/store/store';
 
 export const registration = async (email: string, password: string, username: string) => {
 	const response = await host.post('auth/registration', {
@@ -6,6 +7,11 @@ export const registration = async (email: string, password: string, username: st
 		username: username,
 		password: password
 	});
+
+	const store = useUserStore();
+
+	store.setUser(response.data.user);
+	localStorage.setItem('token', response.data.token);
 
 	return response.data;
 };
