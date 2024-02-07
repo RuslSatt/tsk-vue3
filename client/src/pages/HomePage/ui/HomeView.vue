0f5b991270e99de243c046e31fd0c1dc2cb2575b
Ruslan Sattarov <r8sattarov@mail.ru>
2024-02-07 14:36:12 +0500

    feat: Переадресация пользователя если он не авторизован

--- client/src/pages/HomePage/ui/HomeView.vue
+++ client/src/pages/HomePage/ui/HomeView.vue
@@ -2,6 +2,22 @@
 	<div>Home Page</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useUserStore } from '@/entities/user/model/store/userStore';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const userStore = useUserStore();
+
+const initUser = () => {
+	userStore.initUser();
+	if (!userStore.authUser) {
+		const router = useRouter();
+		router.push('/auth');
+	}
+};
+
+onMounted(initUser);
+</script>
 
 <style scoped></style>
