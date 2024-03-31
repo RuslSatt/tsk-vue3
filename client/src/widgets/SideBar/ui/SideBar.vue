a2b77883801990a0a1ff29ff7b9ed51b38bdab97
Ruslan Sattarov <sattarovro@ufa.infteh.ru>
2024-03-31 18:38:47 +0500

    feat: Добавил Sidebar

--- client/src/widgets/SideBar/ui/SideBar.vue
+++ client/src/widgets/SideBar/ui/SideBar.vue
@@ -1,7 +1,67 @@
 <template>
-	<div></div>
+	<div class="sidebar">
+		<ul class="sidebar__list">
+			<li class="sidebar__item">
+				<a class="sidebar__item-link" href="#">
+					<i class="pi pi-home"></i>
+					<span>Главная</span>
+				</a>
+			</li>
+			<li class="sidebar__item">
+				<a class="sidebar__item-link" href="#">
+					<i class="pi pi-th-large"></i>
+					<span>Проекты</span>
+				</a>
+			</li>
+			<li class="sidebar__item">
+				<a class="sidebar__item-link" href="#">
+					<i class="pi pi-list"></i>
+					<span>Задачи</span>
+				</a>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style scoped></style>
+<style scoped>
+.sidebar {
+	width: 200px;
+	overflow: hidden;
+	height: 100%;
+	border-right: 1px solid var(--surface-300);
+}
+
+@media (max-width: 778px) {
+	.sidebar {
+		position: absolute;
+		transform: translateX(-200px);
+	}
+}
+
+.sidebar__list {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	padding: 10px;
+	gap: 10px;
+}
+
+.sidebar__item-link {
+	display: flex;
+	width: 100%;
+	gap: 10px;
+	padding: 10px;
+	border-radius: 5px;
+	transition: background-color 0.3s;
+	color: var(--text-color);
+	text-decoration: none;
+}
+
+.sidebar__item-link:hover {
+	background-color: var(--highlight-bg);
+	color: var(--highlight-text-color);
+}
+</style>
