8a21fe136563f6682d4c47f5bfd4349f8630d1a3
Ruslan Sattarov <sattarovro@ufa.infteh.ru>
2024-04-05 19:28:12 +0500

    feat: Добавлен фукнционал открытия календаря через поле
    Добавлены стили для элементов задачи

--- client/src/entities/task/ui/TaskPage.vue
+++ client/src/entities/task/ui/TaskPage.vue
@@ -1,20 +1,27 @@
 <template>
-	<div v-if="taskStore.selectedTask" :class="{ open: taskStore.isOpenPage }" class="task">
+	<div @click="closeItems" v-if="taskStore.selectedTask" :class="{ open: taskStore.isOpenPage }" class="task">
 		<header class="task__header">
 			<p-button @click="closePage" text icon="pi pi-angle-double-right"></p-button>
 		</header>
 		<div class="task__content">
-			<div class="content__item">
+			<div class="content__item_title">
 				<input @input="editTask" v-model="taskStore.selectedTask.name" />
 			</div>
 			<div class="content__item">
-				<span>Due</span>
-				<p-calendar
-					class="content__item_calendar"
-					dateFormat="dd.mm.yy"
-					v-model="taskStore.selectedTask.deadline"
-					@update:modelValue="editTask"
-				></p-calendar>
+				<span class="item__name">Дата</span>
+				<div @click.stop="toggleCalendar" class="item__field">
+					<div class="item__field_value">
+						{{ taskStore.selectedTask.deadline?.toLocaleString() }}
+					</div>
+					<p-calendar
+						class="item__value_calendar"
+						:class="{ open: open }"
+						dateFormat="dd.mm.yy"
+						v-model="taskStore.selectedTask.deadline"
+						@update:modelValue="editTask"
+						inline
+					></p-calendar>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -22,14 +29,25 @@
 
 <script setup lang="ts">
 import { useTaskStore } from '@/features/addTask';
+import { ref } from 'vue';
 
 const taskStore = useTaskStore();
+
+const open = ref(false);
 
 const closePage = () => (taskStore.isOpenPage = false);
 
 const editTask = async () => {
 	if (!taskStore.selectedTask) return;
 	await taskStore.updateTask(taskStore.selectedTask);
+};
+
+const toggleCalendar = () => {
+	open.value = !open.value;
+};
+
+const closeItems = () => {
+	open.value = false;
 };
 </script>
 
@@ -68,13 +86,49 @@ const editTask = async () => {
 	gap: 10px;
 }
 
+.content__item_title {
+	font-size: 32px;
+	margin-bottom: 20px;
+}
+
 .content__item {
 	display: flex;
 	align-items: center;
 	gap: 20px;
+	padding: 5px;
+	height: 40px;
+	width: 100%;
 }
 
-.content__item_calendar {
+.item__field {
+	position: relative;
 	width: 100%;
+	height: 100%;
+	cursor: pointer;
+}
+
+.item__field_value {
+	width: 100%;
+	height: 100%;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	transition: 0.3s;
+	padding: 0 5px;
+}
+
+.item__field_value:hover {
+	background-color: var(--surface-200);
+}
+
+.item__value_calendar {
+	display: none;
+	position: absolute;
+	margin-top: 5px;
+	width: 300px;
+}
+
+.item__value_calendar.open {
+	display: block;
 }
 </style>
